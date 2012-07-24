a7ca82bfe299bc95bd064599f19ac0f00a25e986
mvladk <mvladk>
2012-07-24 23:53:33 +0300

    Added Exercise 01, C# WPFApplication project

---
+++ C12 Ex01 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace C12Ex01Y314440009V319512893
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
