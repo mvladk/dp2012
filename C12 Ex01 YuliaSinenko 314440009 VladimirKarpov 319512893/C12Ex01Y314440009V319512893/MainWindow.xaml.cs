0998d4e945f20cfa20b5ca11b53fc16b1083697f
mvladk <mvladk>
2012-07-27 15:02:13 +0300

    Added login button

--- C12 Ex01 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/MainWindow.xaml.cs
+++ C12 Ex01 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
 
 namespace C12Ex01Y314440009V319512893
 {
@@ -19,10 +21,85 @@ namespace C12Ex01Y314440009V319512893
     /// </summary>
     public partial class MainWindow : Window
     {
+        User m_LoggedInUser;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.Title = "Facebook Photos Browser";
+            this.Title = "Facebook Photos Browser.  DP.H.B12.319512893.314440009";
         }
+
+        private void loginAndInit()
+        {
+            LoginResult result = FacebookService.Login("229916837130733",
+                "user_about_me", "friends_about_me", "publish_stream", "user_events", "read_stream",
+                "user_status" // this is instead of the 'user_checkins' permission, as desricbed here: http://developers.facebook.com/bugs/170251059758531
+                );
+            //,
+            //"user_activities", "friends_activities",
+            //"user_birthday", "friends_birthday",
+            //"user_checkins", "friends_checkins",
+            //"user_education_history", "friends_education_history",
+            //"user_events", "friends_events",
+            //"user_groups" , "friends_groups",
+            //"user_hometown", "friends_hometown",
+            //"user_interests", "friends_interests",
+            //"user_likes", "friends_likes",
+            //"user_location", "friends_location",
+            //"user_notes", "friends_notes",
+            //"user_online_presence", "friends_online_presence",
+            //"user_photo_video_tags", "friends_photo_video_tags",
+            //"user_photos", "friends_photos",
+            //"user_photos", "friends_photos",
+            //"user_relationships", "friends_relationships",
+            //"user_relationship_details","friends_relationship_details",
+            //"user_religion_politics","friends_religion_politics",
+            //"user_status", "friends_status",
+            //"user_videos", "friends_videos",
+            //"user_website", "friends_website",
+            //"user_work_history", "friends_work_history",
+            //"email",
+            //"read_friendlists",
+            //"read_insights",
+            //"read_mailbox",
+            //"read_requests",
+            //"read_stream",
+            //"xmpp_login",
+
+            //"create_event",
+            //"rsvp_event",
+            //"sms",
+            //"publish_checkins",
+            //"manage_friendlists",
+            //"manage_pages",
+
+            //"offline_access"
+
+            if (string.IsNullOrEmpty(result.ErrorMessage))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                fetchUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void fetchUserInfo()
+        {
+            //picture_smallPictureBox.LoadAsync(m_LoggedInUser.PictureNormalURL);
+            if (m_LoggedInUser.Statuses.Count > 0)
+            {
+                //textBoxStatus.Text = m_LoggedInUser.Statuses[0].Message;
+                this.Title = m_LoggedInUser.Statuses[0].Message;
+            }
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            loginAndInit();
+        }
+
     }
 }
