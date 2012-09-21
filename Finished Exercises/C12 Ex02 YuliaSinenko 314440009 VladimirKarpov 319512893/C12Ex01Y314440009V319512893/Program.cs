83d33d757f2a3b0845f044fca4082448096fd918
mvladk <mvladk>
2012-09-22 00:49:59 +0300

    SUBMITTED EX02 some changes

---
+++ Finished Exercises/C12 Ex02 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/Program.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Program.cs" company="Holon Institute of Technology">
+//     Copyright (c) Holon Institute of Technology. All rights reserved.
+// </copyright>
+// <author>319512893 - Vldimir Karpov</author>
+// <author>314440009 - Yulia Sinenko</author>
+
+namespace C12Ex02Y314440009V319512893
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
---
+++ Finished Exercises/C12 Ex02 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/Windows/ProfilePicture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C12Ex01Y314440009V319512893
+{
+    class ProfilePicture
+    {
+        private PictureBox m_UserProfilePictureBox;
+
+        public PictureBox PictureBox
+        {
+            get { return m_UserProfilePictureBox; }
+            set { m_UserProfilePictureBox = value; }
+        }
+
+    }
+}
