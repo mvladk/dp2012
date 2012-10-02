5a5f2a668e6b461f1e107006a75ca58cccc8fc27
mvladk <mvladk>
2012-10-02 16:21:43 +0200

    minor

--- C12 Ex03 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/Comparer.cs
+++ C12 Ex03 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/Comparer.cs
@@ -9,26 +9,8 @@ namespace C12Ex03Y314440009V319512893
     /// the strategy declaration
     public abstract class Comparer
     {
-        abstract public bool ShouldSwap(object i_Num1, object i_Num2);
+        abstract public bool ShouldSwap(object i_Comparable1, object i_Comparable2);
     }
-
-    /// a strategy implementation
-    //public class ComparerDown : Comparer
-    //{
-    //    public override bool ShouldSwap(int i_Num1, int i_Num2)
-    //    {
-    //        return i_Num1 > i_Num2;
-    //    }
-    //}
-
-    ///// another strategy implementation
-    //public class ComparerUp : Comparer
-    //{
-    //    public override bool ShouldSwap(int i_Num1, int i_Num2)
-    //    {
-    //        return i_Num1 < i_Num2;
-    //    }
-    //}
 
     public class ComparerDownAlbumsByPhotosCount : Comparer
     {
