1d6fc41657d7f693193b2bd39da36c353cfa2e73
mvladk <mvladk>
2012-10-02 17:06:52 +0200

    added iterator interfaces

---
+++ C12 Ex03 YuliaSinenko 314440009 VladimirKarpov 319512893/C12Ex01Y314440009V319512893/Classes/IIterator.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IIterator.cs" company="Holon Institute of Technology">
+//     Copyright (c) Holon Institute of Technology. All rights reserved.
+// </copyright>
+// <author>319512893 - Vldimir Karpov</author>
+// <author>314440009 - Yulia Sinenko</author>
+namespace C12Ex03Y314440009V319512893
+{
+    /// The Iterator interface
+    public interface IIterator
+    {
+        bool Next();
+        object CurrentItem { get; } /// polymorphic item reference
+        bool IsDone { get; }
+        void Reset();
+    }
+
+    /// The Aggregte interface
+    public interface IAggregate
+    {
+        IIterator CreateIterator();
+    }
+}
